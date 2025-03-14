74941be0877c4d60c2f53886243162c8726044f9
Andrew Hale <services.hale.adh@gmail.com>
2025-03-13 23:10:01 -0400

    remove windows support

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
 // Import from a local module
-import Example from "./modules/example.js";
+import Example from './modules/example.js';
 
 // Import from a node module, this will be rolled-up
 // so you don't need to include a node_modules dir in prod
-import Cookies from "js-cookie";
+import Cookies from 'js-cookie';
 
 // instntiate a new class instance
-new Example("Logged from the main script.");
+new Example('Logged from the main script.');
 
 // use an imported method from a node_module
-Cookies.set("foo", "bar");
+Cookies.set('foo', 'bar');
